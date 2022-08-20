dd1aa61d64215f70ae41e547dd5684029681fd89
Akshat Sethi <akshat@nova-benefits.com>
2022-08-20 13:14:57 +0530

    done

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  return (
+    <div id="main">
+      <input type="text"/>
+      <button >Add</button>
+      <div id="todo-list">
+
+      
+      </div>  
+
+    </div>
+  )
+}
+
+
+export default App;
